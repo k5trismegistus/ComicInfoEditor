b93798fba681ce5dabd7941eb128de15ff2e9eba
k5.trismegistus <k5.trismegistus@gmail.com>
2014-08-16 11:07:46 +0900

    XMLの編集まで可能に。ただしもとからある要素しか変更できず、今まで設定されていなかった要素の追記ができない。

--- ComicInfoXmlHandler.py
+++ ComicInfoXmlHandler.py
@@ -1,23 +1,42 @@
 import zipfile
 import xml.etree.ElementTree as etree
+import io
 
 
 
 class ComicInfoXmlHandler():
 
-    def __init__(self, fromxml):
+    def __init__(self, fromxml, filepath):
         self.fromxml = fromxml
-        self.todisplay = {}
 
-    def get_info(self, filepath):
         with zipfile.ZipFile(filepath, mode='r',) as z:
             comicinfoxml = z.open('ComicInfo.xml', mode='r')
-            infotree = etree.parse(comicinfoxml)
-            root = infotree.getroot()
-            for i in self.fromxml:
-                self.todisplay[i] = root.findtext(i)
+            self.infotree = etree.parse(comicinfoxml)
+            self.root = self.infotree.getroot()
 
-            print(self.todisplay)
+    def get_info(self):
+        todisplay = {}
+        for i in self.fromxml:
+            todisplay[i] = self.root.findtext(i)
+        return todisplay
+
+    def generate_xml(self, toxml):
+        g = io.StringIO()
+        for i in toxml:
+            if (toxml[i] != '') & (self.root.find(i) is not None):
+                self.root.find(i).text = toxml[i]
+            elif (toxml[i] != '') & (self.root.find(i) is None):
+                newelem = etree.Element(i)
+                newelem.text = toxml[i]
+                self.root.find('ComicInfo').append(newelem)
+            elif toxml[i] == '':
+                self.root.find(i).remove
+        self.infotree.write(g, encoding='unicode')
+        return g
+
+    def write_info(self, xml2write, filepath):
+        with zipfile.ZipFile(filepath, mode='w',) as z:
+            z.writestr('ComicInfo.xml', xml2write.getvalue())
 
 
 if __name__ == '__main__':
--- GuiWindow.py
+++ GuiWindow.py
@@ -5,7 +5,7 @@ import ComicInfoXmlHandler
 class ComicInfoGetter(wx.App):
 
     def OnInit(self):
-        frm = GuiWindow("ComicInfo Getter from D&M Lexicon")
+        frm = GuiWindow("ComicInfoEditor")
         frm.Show()
         return 1
 
@@ -14,8 +14,7 @@ class GuiWindow(wx.Frame):
 
     def __init__(self, title):
 
-        self.fromxml = ['Series', 'Number', 'Title', 'Writer', 'Penciller', 'Genre', 'Year', 'Month', 'Day']
-        self.cix = ComicInfoXmlHandler.ComicInfoXmlHandler(self.fromxml)
+        self.field_of_comicinfoxml = ['Series', 'Number', 'Title', 'Writer', 'Penciller', 'Genre', 'Year', 'Month', 'Day']
 
         wx.Frame.__init__(self, None, -1, title, size=(480, 600), pos=(400, 448))
         panel = wx.Panel(self, wx.ID_ANY)
@@ -23,6 +22,9 @@ class GuiWindow(wx.Frame):
         self.lexicon_url_input = wx.TextCtrl(panel, wx.ID_ANY, 'Info URL')
         self.filepath = wx.TextCtrl(panel, wx.ID_ANY , 'Archive Path', style=wx.TE_READONLY)
         self.openfilebutton = wx.Button(panel, wx.ID_ANY, '...')
+
+        self.savebutton = wx.Button(panel, wx.ID_ANY, 'Save XML to Archive')
+        self.resetbutton = wx.Button(panel, wx.ID_ANY, 'Reset Editing')
 
         self.editorfield_series = wx.TextCtrl(panel, wx.ID_ANY, '')
         self.editorfield_number = wx.TextCtrl(panel, wx.ID_ANY, '')
@@ -46,8 +48,10 @@ class GuiWindow(wx.Frame):
             self.editorfield_day
         ]
 
-        self.openfilebutton.Bind(wx.EVT_BUTTON, self.open)
-        self.filepath.Bind(wx.EVT_TEXT, self.loadcomicinfo)
+        self.openfilebutton.Bind(wx.EVT_BUTTON, self.open_archive)
+        self.filepath.Bind(wx.EVT_TEXT, self.load_comicinfo)
+        self.savebutton.Bind(wx.EVT_BUTTON, self.save_comicinfo)
+        self.resetbutton.Bind(wx.EVT_BUTTON, self.load_comicinfo)
 
         dt = Droptarget(self)
         self.SetDropTarget(dt)
@@ -57,11 +61,12 @@ class GuiWindow(wx.Frame):
         line_series = wx.BoxSizer(wx.HORIZONTAL)
         line_artist = wx.BoxSizer(wx.HORIZONTAL)
         line_others = wx.BoxSizer(wx.HORIZONTAL)
+        line_saveloadbuttons = wx.BoxSizer(wx.HORIZONTAL)
 
         layout = wx.BoxSizer(wx.VERTICAL)
 
         line_filepath.Add(self.filepath, proportion=1, flag=wx.GROW)
-        line_filepath.Add(self.openfilebutton, proportion=0, flag=wx.GROW)
+        line_filepath.Add(self.openfilebutton, proportion=0)
 
         line_lexiconurl.Add(self.lexicon_url_input, proportion=1, flag=wx.GROW)
 
@@ -77,15 +82,19 @@ class GuiWindow(wx.Frame):
         line_others.Add(self.editorfield_month, proportion=0, flag=wx.GROW)
         line_others.Add(self.editorfield_day, proportion=0, flag=wx.GROW)
 
+        line_saveloadbuttons.Add(self.savebutton, proportion=0)
+        line_saveloadbuttons.Add(self.resetbutton, proportion=0)
+
         layout.Add(line_filepath, proportion=0, flag=wx.ALIGN_RIGHT | wx.EXPAND | wx.ALL, border=5)
         layout.Add(line_lexiconurl, proportion=0, flag=wx.ALIGN_RIGHT | wx.EXPAND | wx.ALL, border=5)
         layout.Add(line_series, proportion=0, flag=wx.ALIGN_RIGHT | wx.EXPAND | wx.ALL, border=5)
         layout.Add(line_artist, proportion=0, flag=wx.ALIGN_RIGHT | wx.EXPAND | wx.ALL, border=5)
         layout.Add(line_others, proportion=0, flag=wx.ALIGN_RIGHT | wx.EXPAND | wx.ALL, border=5)
+        layout.Add(line_saveloadbuttons, proportion=0, flag=wx.ALIGN_CENTER | wx.ALL, border=5)
 
         panel.SetSizer(layout)
 
-    def open(self, e):
+    def open_archive(self, e):
         path = ''
         dialog = wx.FileDialog(self, "Choose a File", path, "*.*")
         if dialog.ShowModal() == wx.ID_OK:
@@ -93,13 +102,22 @@ class GuiWindow(wx.Frame):
             self.filepath.SetValue(path)
         dialog.Destroy()
 
-    def loadcomicinfo(self, e):
+    def load_comicinfo(self, e):
+        self.cix = ComicInfoXmlHandler.ComicInfoXmlHandler(self.field_of_comicinfoxml, self.filepath.GetValue())
+
         for i in range(len(self.edirotfield_list)):
             self.edirotfield_list[i].SetValue('')
-        self.cix.get_info(self.filepath.GetValue())
+        todisplay = self.cix.get_info()
         for i in range(len(self.edirotfield_list)):
-            if self.cix.todisplay[self.fromxml[i]] != None:
-                self.edirotfield_list[i].SetValue(self.cix.todisplay[self.fromxml[i]])
+            if todisplay[self.field_of_comicinfoxml[i]] is not None:
+                self.edirotfield_list[i].SetValue(todisplay[self.field_of_comicinfoxml[i]])
+
+    def save_comicinfo(self, e):
+        toxml = {}
+        for i in range(len(self.edirotfield_list)):
+            if self.edirotfield_list[i].GetValue() != '':
+                toxml[self.field_of_comicinfoxml[i]] = self.edirotfield_list[i].GetValue()
+        self.cix.write_info(self.cix.generate_xml(toxml), self.filepath.GetValue())
 
 
 class Droptarget(wx.FileDropTarget):
